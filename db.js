c147216f3a613d054b9c4ba3ea9551e5bb4a931a
ckrandhir <ckrandhir@hotmail.com>
2020-05-25 12:48:04 -0400

    db change

--- db.js
+++ db.js
@@ -1,28 +1,9 @@
 const mongoose = require("mongoose");
 
-
 //Mlab
 
-mongoose.connect(
-  "mongodb+srv://chandan:help4you@cluster0-yawar.mongodb.net/ck?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  },
-  (err) => {
-    if (!err) {
-      console.log("connected successfully");
-    } else {
-      console.log(
-        "errror in Db connection :-" + JSON.stringify(err, undefined, 2)
-      );
-    }
-  }
-);
-
-//Mongo Atlas
-
 // mongoose.connect(
-//   "mongodb+srv://chandan:help4you@cluster0-r4yln.mongodb.net/Chandan?retryWrites=true&w=majority", {
+//   "mongodb+srv://chandan:help4you@cluster0-yawar.mongodb.net/ck?retryWrites=true&w=majority", {
 //     useNewUrlParser: true,
 //     useUnifiedTopology: true
 //   },
@@ -36,5 +17,24 @@ mongoose.connect(
 //     }
 //   }
 // );
+
+//Mongo Atlas
+
+mongoose.connect(
+  "mongodb+srv://chandan:help4you@cluster0-tx6h1.mongodb.net/chandan?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (!err) {
+      console.log("connected successfully");
+    } else {
+      console.log(
+        "errror in Db connection :-" + JSON.stringify(err, undefined, 2)
+      );
+    }
+  }
+);
 
 module.export = mongoose;
