126931b4acfd2c077e5f1ef7ac56e7d06e20fbf3
ckrandhir <ckrandhir@hotmail.com>
2020-05-25 16:28:35 -0400

    add cors url

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ var employeeController = require("./controllers/employeeController.js");
 var port = process.env.PORT || 8080;
 var app = express();
 app.use(bodyParser.json());
-app.use(cors({ origin: "http://localhost:4200" }));
+app.use(
+  cors(
+    { origin: "http://localhost:4200" },
+    { origin: "https://ck-node-heroku.web.app" }
+  )
+);
 
 app.listen(
   port,
